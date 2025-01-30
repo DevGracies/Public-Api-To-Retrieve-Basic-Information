b8bed3a5eea3ccc346b48140be482c1ab49cadc5
Devgraices <graceadegunle16@gmail.com>
2025-01-30 18:14:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/api/information", (req, res) => {
+  const response = {
+    email: process.env.EMAIL,
+    curerent_datetime: new Date().toISOString(),
+    github_url: process.env.github_url,
+  };
+  res.json(response);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+console.log(new Date());
+console.log(new Date().toISOString());
